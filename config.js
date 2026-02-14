a5728ec8abea8245ce420ea5dccde93ca93e8b39
Mattadata <mattangelo2@gmail.com>
2026-02-14 15:13:15 -0600

    feat: add Google Vision API key for deployment

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Configuration file for Fuelly OCR App
+ * Add your API keys here - this file is NOT committed to git
+ */
+
+const CONFIG = {
+  // Google Cloud Vision API Key
+  // Get yours from: https://console.cloud.google.com/apis/credentials
+  googleVisionApiKey: 'AIzaSyBxer136SO25uBAIsOpikWTOHEf9Id5oDY'
+};
